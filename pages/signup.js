8b6aab99553086ed0af6a0f6ff00306d57757cb4
anshika2306 <anshika.mahajan10@gmail.com>
2022-11-03 11:15:03 +0530

    signup api and initial function

--- pages/signup.js
+++ pages/signup.js
@@ -1,33 +1,91 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
-const signup = () => {
+const Signup = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const handleChange = (e) => {
+    if (e.target.name == 'name') {
+      setName(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name == 'password') {
+      setPassword(e.target.value)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = { name, email, password }
+
+    let res = await fetch('http://localhost:3000/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    let response = await res.json()
+    console.log(response)
+    setName('')
+    setEmail('')
+    setPassword('')
+    toast.success('Your account has been created!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+  }
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
           <Image width={100} height={40} className="mx-auto h-12 w-auto" src="/digiaccel.svg" alt="Digiaccel logo" />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up for an account</h2>
-          <p class="mt-2 text-center text-sm text-gray-600">
+          <p className="mt-2 text-center text-sm text-gray-600">
             Or
-            <Link href="/login" class="font-medium text-indigo-600 hover:text-indigo-500"> Login</Link>
+            <Link href="/login" className="font-medium text-indigo-600 hover:text-indigo-500"> Login</Link>
           </p>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div className="-space-y-px rounded-md shadow-sm">
             <div>
-              <label for="full-name" className="sr-only">Full Name</label>
-              <input id="full-name" name="full-name" type="text" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Your Full Name" />
+              <label htmlFor="name" className="sr-only">Full Name</label>
+              <input value={name} onChange={handleChange} id="name" name="name" type="text" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Your Full Name" />
             </div>
             <div>
-              <label for="email-address" className="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+              <label htmlFor="email" className="sr-only">Email address</label>
+              <input value={email} onChange={handleChange} id="email" name="email" type="email" autocomplete="email" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
             </div>
             <div>
-              <label for="password" className="sr-only">Password</label>
-              <input id="password" name="password" type="password" autocomplete="current-password" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+              <label htmlFor="password" className="sr-only">Password</label>
+              <input value={password} onChange={handleChange} id="password" name="password" type="password" autocomplete="current-password" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
             </div>
           </div>
 
@@ -49,4 +107,4 @@ const signup = () => {
   )
 }
 
-export default signup
+export default Signup
